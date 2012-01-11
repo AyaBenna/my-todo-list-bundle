eb20744cb9d5a89c72540884e5e63b4a819e92b3
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-01-11 09:48:55 +0000

    Building the menu and changing colors

---
+++ src/My/TodoListBundle/Menu/Builder.php
@@ -0,0 +1,26 @@
+<?php
+namespace My\TodoListBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setCurrentUri($this->container->get('request')->getRequestUri());
+
+        $menu->addChild('Home', array('route' => 'homepage'));
+        $menu->addChild('Tasks', array('route' => 'task_list'));
+        $menu->addChild('Statuses', array('route' => 'status_list'));
+        $menu->addChild('Priorities', array('route' => 'priority_list'));
+
+        foreach ($menu as &$item)
+        {
+            $item->setLinkAttribute('class', 'button');
+        }
+
+        return $menu;
+    }
+}
